2941090a756e239a53ee27b8f77dab66497f3ad0
Lyon_Yan <765211630@qq.com>
2015-11-28 09:36:02 +0800

    update

--- src/com/lyon_yan/module/wxpay/bean/close_order/CloseOrderReqData.java
+++ src/com/lyon_yan/module/wxpay/bean/close_order/CloseOrderReqData.java
@@ -5,8 +5,6 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-import android.util.Log;
-
 import com.lyon_yan.module.wxpay.config.Configure;
 import com.lyon_yan.module.wxpay.core.RandomStringGenerator;
 import com.lyon_yan.module.wxpay.core.Signature;
